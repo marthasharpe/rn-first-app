b459f5dac1dbdeba17423c98d0ac73bac34f8d5e
Martha Sharpe <marthasharpe2020@gmail.com>
2020-03-18 22:00:14 -0400

    simple counter buttons

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Change Text"/>
+      <Text>Count: {count}</Text>
+      <Button title="Increment" onPress={() => setCount(count + 1)} />
+      <Button title="Decrement" onPress={() => setCount(count - 1)} />
+
     </View>
   );
 }
