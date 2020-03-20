bbd9a94ada3d6f1f35eb9a9b8893f70090e47f68
Martha Sharpe <marthasharpe2020@gmail.com>
2020-03-20 08:04:09 -0500

    handle input and add goals

--- App.js
+++ App.js
@@ -2,12 +2,35 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [newGoal, setNewGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const handleInput = (input) => {
+    setNewGoal(input);
+  }
+
+  const addNewGoal = () => {
+    // this syntax guarantees that it is the current state snapshot
+    setCourseGoals(currentGoals => [...currentGoals, newGoal])
+    // clear input field
+    setNewGoal('')
+  }
 
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input}/>
-        <Button title="ADD" />
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={handleInput}
+          value={newGoal}
+          />
+        <Button title="+" onPress={addNewGoal}/>
+      </View>
+      <View>
+        {courseGoals.map(goal => (
+          <Text>{goal}</Text>
+        ))}
       </View>
     </View>
   );
